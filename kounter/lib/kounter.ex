1a84e6d41fb6f7a31a38118192586e2696835954
Vijay Chakilam <vijaychakilam@Vijays-MBP.fios-router.home>
2023-08-23 11:07:05 -0400

    Kounter module

--- kounter/lib/kounter.ex
+++ kounter/lib/kounter.ex
@@ -1,18 +1,23 @@
 defmodule Kounter do
-  @moduledoc """
-  Documentation for `Kounter`.
-  """
+	defstruct [kount: 0]
 
-  @doc """
-  Hello world.
+	def new(val) when is_integer(val) do
+		%__MODULE__{kount: val}
+	end
 
-  ## Examples
+	def inc(%__MODULE__{kount: val}=new_kounter) do
+		%{new_kounter | kount: val + 1}
+	end
 
-      iex> Kounter.hello()
-      :world
+	def dec(%__MODULE__{kount: val}=new_kounter) do
+		%{new_kounter | kount: val - 1}
+	end
 
-  """
-  def hello do
-    :world
-  end
+	def add(%__MODULE__{kount: val}=new_kounter, x) when is_integer(x) do
+		%{new_kounter | kount: val + x}
+	end
+
+	def show(data) do
+		inspect data.kount
+	end
 end
