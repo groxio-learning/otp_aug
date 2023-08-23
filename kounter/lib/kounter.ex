f703f8dc8b274acba1c1318421379ec1f6509e15
Vijay Chakilam <vijaychakilam@Vijays-MBP.fios-router.home>
2023-08-23 10:30:52 -0400

    init kounter

---
+++ kounter/lib/kounter.ex
@@ -0,0 +1,18 @@
+defmodule Kounter do
+  @moduledoc """
+  Documentation for `Kounter`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Kounter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
